8a99a0762a5c94e2e91e7dfd8c868c47a22983b4
Netanel Chertkov <netanelchertkov@gmail.com>
2023-10-20 10:29:04 +0300

    Add cursor animation

--- app.js
+++ app.js
@@ -54,3 +54,65 @@ document.addEventListener("DOMContentLoaded", function() {
         loop: true
     });
 });
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+    '#007acc',
+    '#007acc',
+    '#0043bf',
+    '#0027b8',
+    '#000ab2',
+    '#1407a8',
+    '#2c05a0',
+    '#420397',
+    '#59008f',
+    '#6f007e',
+    '#85006e',
+    '#9b005d',
+    '#b1004c',
+    '#c7003b',
+    '#dd002b',
+    '#f3001a',
+    '#ff0e0d',
+    '#ff3a13',
+    '#ff7f50',
+    '#ff7f50'
+  ];  
+  
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
